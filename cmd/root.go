c579ea5480513a305113d6dbf43cdb760d33914d
John Long <xadhatter@pm.me>
2023-05-24 21:47:01 -0400

    added ability to set flags with env vars prefixed with FOX_

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,12 @@ package cmd
 
 import (
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"github.com/xigxog/kubefox-cli/internal/config"
 	"github.com/xigxog/kubefox-cli/internal/log"
 	"github.com/xigxog/kubefox/libs/core/admin"
@@ -18,8 +21,9 @@ var (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "fox",
-	Short: "CLI for interacting with KubeFox",
+	Use:              "fox",
+	PersistentPreRun: initViper,
+	Short:            "CLI for interacting with KubeFox",
 	Long: `
 🦊 Fox is a CLI for interacting with KubeFox. You can use it to create, build, 
 validate, deploy, and release your KubeFox components, apps, and systems.
@@ -31,6 +35,19 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&flags.URL, "url", "u", "", "url to the KubeFox API")
 	rootCmd.PersistentFlags().StringVarP(&flags.OutFormat, "output", "o", "yaml", `output format. One of: "json", "yaml"`)
 	rootCmd.PersistentFlags().BoolVarP(&flags.Verbose, "verbose", "v", false, "enable verbose output")
+}
+
+func initViper(cmd *cobra.Command, args []string) {
+	viper.SetEnvPrefix("fox")
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+
+	viper.BindPFlags(cmd.Flags())
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		if f.Value.String() == f.DefValue && viper.IsSet(f.Name) && viper.GetString(f.Name) != "" {
+			cmd.Flags().Set(f.Name, viper.GetString(f.Name))
+		}
+	})
 }
 
 func Execute() {
